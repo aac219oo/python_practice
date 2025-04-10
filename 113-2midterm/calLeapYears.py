b2b65604dab65578933c6606aee0ee19015ebcb5
James Hsu <james.hsu@twjoin.com>
2025-04-10 21:46:59 +0800

    拼寫錯誤修正

--- 113-2midterm/calLeapYears.py
+++ 113-2midterm/calLeapYears.py
@@ -13,9 +13,9 @@ images\113-2midterm\calLeapYears.jpg
 
 # inputFirstYear = int(input("請輸入第1個年份："))
 # inputSecondYear = int(input("請輸入第2個年份："))
-# yearsContainer = [inputFirstYear, inputSecondYear]
-# leapYaerCount = 0
-# leapYaersContainer = []
+# yearsList = [inputFirstYear, inputSecondYear]
+# leapYearCount = 0
+# leapYearsList = []
 
 # def isAD(year):
 #     if year > 0:
@@ -36,37 +36,37 @@ images\113-2midterm\calLeapYears.jpg
 #         message = [False, "請輸入正確年分"]
 #         return message
 
-# for i in range(len(yearsContainer)):
-#     if isAD(yearsContainer[i]):
-#         print(f"西元{yearsContainer[i]}年{isLeapYearFn(yearsContainer[i])[1]}閏年")
+# for i in range(len(yearsList)):
+#     if isAD(yearsList[i]):
+#         print(f"西元{yearsList[i]}年{isLeapYearFn(yearsList[i])[1]}閏年")
 #     else:
-#         print(f"第{(i + 1)}個年份非西元後，{isLeapYearFn(yearsContainer[i])[1]}")
+#         print(f"第{(i + 1)}個年份非西元後，{isLeapYearFn(yearsList[i])[1]}")
 
 # if isAD(inputFirstYear) & isAD(inputSecondYear):
 #     for i in range(inputFirstYear, inputSecondYear + 1):
 #             if isLeapYearFn(i)[0]:
-#                 leapYaersContainer.append(i)
-#                 leapYaerCount += 1
-#     print(f"西元{inputFirstYear}年到西元{inputSecondYear}年之間，{leapYaersContainer}為閏年")
-#     print(f"西元{inputFirstYear}年到西元{inputSecondYear}年總共有{leapYaerCount}個閏年")
+#                 leapYearsList.append(i)
+#                 leapYearCount += 1
+#     print(f"西元{inputFirstYear}年到西元{inputSecondYear}年之間，{leapYearsList}為閏年")
+#     print(f"西元{inputFirstYear}年到西元{inputSecondYear}年總共有{leapYearCount}個閏年")
 
 
-yearsContainer = []
-leapYaerCount = 0
-leapYaersContainer = []
+yearsList = []
+leapYearCount = 0
+leapYearsList = []
 
 for i in range(2):
     while True:
         try:
             inputYear = int(input(f"請輸入第{i+1}個年份："))
             if inputYear > 0:
-                yearsContainer.append(inputYear)
+                yearsList.append(inputYear)
                 break
             else:
                 print("請輸入西元後")
         except ValueError:
             print("請輸入有效正整數")
-yearsContainer.sort()
+yearsList.sort()
 
 def isLeapYearFn(year):
     message = []
@@ -77,12 +77,12 @@ def isLeapYearFn(year):
         message = [isLeapYear, "不是"]
     return message
 
-for i in range(len(yearsContainer)):
-        print(f"西元{yearsContainer[i]}年{isLeapYearFn(yearsContainer[i])[1]}閏年")
+for i in range(len(yearsList)):
+        print(f"西元{yearsList[i]}年{isLeapYearFn(yearsList[i])[1]}閏年")
 
-for i in range(yearsContainer[0], yearsContainer[1] + 1):
+for i in range(yearsList[0], yearsList[1] + 1):
         if isLeapYearFn(i)[0]:
-            leapYaersContainer.append(i)
-            leapYaerCount += 1
-print(f"西元{yearsContainer[0]}年到西元{yearsContainer[1]}年之間，{leapYaersContainer}為閏年")
-print(f"西元{yearsContainer[0]}年到西元{yearsContainer[1]}年總共有{leapYaerCount}個閏年")
+            leapYearsList.append(i)
+            leapYearCount += 1
+print(f"西元{yearsList[0]}年到西元{yearsList[1]}年之間，{leapYearsList}為閏年")
+print(f"西元{yearsList[0]}年到西元{yearsList[1]}年總共有{leapYearCount}個閏年")
